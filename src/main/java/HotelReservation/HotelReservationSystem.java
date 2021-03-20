3323c6aa0afb18c91d1e156d22109ed926b9e6e3
0vishal <vishalsalaskar72@gmail.com>
2021-03-20 07:36:16 +0530

    Add Rating to each hotel

--- src/main/java/HotelReservation/HotelReservationSystem.java
+++ src/main/java/HotelReservation/HotelReservationSystem.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 
 public class HotelReservationSystem {
 
-       public static ArrayList<String> AddHotel(String hotel) {
-           System.out.println("Welcome to Hotel Reservation");
-           ArrayList<String> addhotel = new ArrayList<>();
-           addhotel.add(hotel);
-           return addhotel;
-       }
-   }
+    public static ArrayList<String> AddHotel(String hotel) {
+        System.out.println("Welcome to Hotel Reservation");
+        public static ArrayList<String> addhotel = new ArrayList<>();
+        addhotel.add(hotel);
+        return addhotel;
+    }
+
+}
--- src/test/java/HotelReservationTest/HotelReservationTest.java
+++ src/test/java/HotelReservationTest/HotelReservationTest.java
@@ -5,6 +5,13 @@ import HotelReservation.HotelReservationSystem;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import java.util.*;
+
+
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HotelReservationTest {
 
@@ -24,12 +31,11 @@ public class HotelReservationTest {
     }
 
 
-        HotelReservation lakewood = new HotelReservation("Lakewood", 100, 120);
-        HotelReservation Bridgewood = new HotelReservation("Bridgewood", 200, 240);
-        HotelReservation Ridgewood = new HotelReservation("Ridgewood", 300, 350);
+        HotelReservation lakewood = new HotelReservation("Lakewood", 100, 120,3);
+        HotelReservation Bridgewood = new HotelReservation("Bridgewood", 200, 240,4);
+        HotelReservation Ridgewood = new HotelReservation("Ridgewood", 300, 350,5);
 
 
-l
     @Test
     public void TestData() {
         Assertions.assertEquals("Lakewood","Lakewood");
@@ -51,4 +57,6 @@ l
         Assertions.assertEquals(240,Bridgewood.weekendrate);
         Assertions.assertEquals(350,Ridgewood.weekendrate);
     }
+
+
 }
